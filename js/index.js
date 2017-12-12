83bc6b62dee07bb2ef170445da136754da7a7c20
Survenator <landsurveyingjobber@gmail.com>
2017-12-12 15:31:23 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+jQuery(window).on('load', function(){
+
+	// PRELOADER
+	jQuery(".img-preloader").fadeOut(1000);
+	jQuery('.megamenu-content a:first-of-type').css('min-height', 'auto');
+
+}); // end window load function
